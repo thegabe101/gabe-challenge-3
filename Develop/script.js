dd21e15cbc434e3d03cde9cb99e936e4c8fb9556
Rayncor <sowa332@gmail.com>
2022-06-25 20:20:50 -0700

    made initial popup window appear, commented out starter code and readded some parts

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,30 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+/*
 
-// Write password to the #password input
+function writePassword() {
+  // 1. prompt user for parameters
+  // 2. length, special chars, numbers, uppercase, lowercase
+  // 3. create random from password from available characters
+  // 4. for loop run the number of times that user sets length to 
+  // add random character to password string
+return password
+}
+
+var generateBtn = document.querySelector("#generate");
+*/ 
+
+var generateBtn = document.querySelector("#generate");
+  generateBtn.addEventListener('click', writePassword);
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//experimental code
+
+//took me a long time to figure out that the add event listener needs to be IN the function
+function generatePassword() {
+  alert("You will now select the criteria for your password.");
+  return password;
+};
